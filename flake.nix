52d741db92e9bfadea38e87c183be1d455b2e82f
Mattias Matthiesen <mattmatthi@hotmail.com>
2024-10-20 15:11:55 +0200

    Configure nix devenv

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs = inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = nixpkgs.lib.systems.flakeExposed;
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devenv.shells.default = {
+          packages = with pkgs; [ maturin ];
+          languages.python.enable = true;
+          languages.rust.enable = true;
+        };
+      };
+    };
+}
